2b537dc1c2650b5c84182de1601d0d12d45874ed
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-09-11 21:21:26 -0700

    set message value to empty when success

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,20 +1,27 @@
+function createANewPost(csrf_token, message) {
+  fetch("/new-post", {
+    method: "POST",
+    message: message,
+    headers: { "X-CSRFToken": csrf_token },
+    body: JSON.stringify({
+      message: message.value,
+    }),
+  })
+    .then((reponse) => reponse.json())
+    .then((result) => {
+		console.log(result);
+		if(result.message) {
+			message.value = "";
+		}
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("form#create_a_new_post").onsubmit = function (e) {
     e.preventDefault();
     const csrf_token = document.getElementsByName("csrfmiddlewaretoken")[0]
       .value;
-    const message = document.querySelector("#post_message");
-    fetch("/new-post", {
-      method: "POST",
-      message: message,
-      headers: { "X-CSRFToken": csrf_token },
-	  body: JSON.stringify({
-		message: message.value
-	  })
-    })
-      .then((reponse) => reponse.json())
-      .then((result) => {
-        console.log(result);
-      });
+    var message = document.querySelector("#post_message");
+    createANewPost(csrf_token, message);
   };
 });
