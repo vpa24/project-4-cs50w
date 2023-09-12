d719559d1a70130ecc9347710f3b897bd3a1aba7
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-09-11 20:04:35 -0700

    create basic social media database structure

--- network/models.py
+++ network/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
     pass
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_posts")
+    message = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_posts')
+
+    def __str__(self):
+        return self.message
